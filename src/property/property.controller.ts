dfcfef6c4fc1c324773796674495099c13543b04
Prisca <priscamasereli@gmail.com>
2025-12-09 12:41:27 +0200

    feat: A body of a request

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
-    @Get()
-    findAll(){
-        return "All properties";
-    }
-    @Get(':id/:slug')
-    findOne(@Param('id') id, @Param('slug') slug){
-        return `id=${id}, slug=${slug}`
-    }
+  @Get()
+  findAll() {
+    return 'All properties';
+  }
+  @Get(':id/:slug')
+  findOne(@Param('id') id, @Param('slug') slug) {
+    return `id=${id}, slug=${slug}`;
+  }
+  // A body of a request
 
-    @Post()
-    create(){
-        return"This will create a property"
-    }
+  // @Post()
+  // create(@Body() body){
+  //     return body;
+  // }
+
+  @Post()
+  create(@Body('name') name) {
+    return name;
+  }
 }
